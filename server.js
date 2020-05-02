84d5aab6a3a871a2f4cbc0fcd4fa3effa490d596
unknown <maggie.aytac@gmail.com>
2020-05-01 17:33:44 -0700

    changed host

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: 'postgresql-animated-96384',
-		user: 'postgres',
-		password: 'goldfish',
-		database: 'smartbrain'
+		host: process.env.DATABASE_URL,
+		ssl: true
 	}
 });
 
