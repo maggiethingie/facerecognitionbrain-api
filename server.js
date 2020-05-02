189cdc19f083d7ec73119bc30b83938bddcdd1a7
unknown <maggie.aytac@gmail.com>
2020-05-02 00:44:11 -0700

    setting node tls reject unauthorized to zero

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
 	client: 'pg',
 	connection: {
-		connectionString: 'postgresql-animated-96384',
+		connectionString: process.env.DATABASE_URL,
 		ssl: true
 	}
 });
